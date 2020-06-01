59f2968720c9d978af39512a79eaacdc17a40c28
hassaankhan235 <hassaankhan235@gmail.com>
2020-06-01 15:35:41 +0300

    final Commit

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 var button = document.getElementById('btn')
-var ul =    document.getElementById('td')
+var sec =    document.getElementById('td')
 var txtbx = document.getElementById('enter')
 var div = document.getElementsByTagName('div')
 
@@ -25,18 +25,19 @@ function add() {
     var val = gettxt()
     if (txtbx.value.length != 0)
     {
-    var li = document.createElement("input")
-    li.type = "checkbox"
-    li.value = "test"
-    li.id = "test"
+    var chkbx = document.createElement("input")
+    chkbx.type = "checkbox"
+    chkbx.value = "test"
+    chkbx.id = "test"
     label = document.createElement("label")
+    label.appendChild(chkbx)
     label.appendChild(document.createTextNode(" " +val))
     br = document.createElement('br')
-    li.appendChild(document.createTextNode("Test"))
+    //chkbx.appendChild(document.createTextNode("Test"))
     //li.addEventListener("click",tog)
-    ul.appendChild(li)
-    ul.appendChild(label)
-    ul.appendChild(br)
+    //sec.appendChild(chkbx)
+    sec.appendChild(label)
+    sec.appendChild(br)
     settxtEmpty()
     console.log("ul node type is " + ul.nodeName +
             "li node name is" + li.nodeName)
